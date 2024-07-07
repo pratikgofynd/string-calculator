13acac4b0c84053b6ad42432bab81011fc335098
pratikgofynd <pratikhadvani@gofynd.com>
2024-07-07 14:24:04 +0530

    added test case for supporting custom delimiter which is failing as expected

--- string-calculator.js
+++ string-calculator.js
@@ -2,7 +2,13 @@ function add(numbers) {
   if (numbers === "") {
     return 0;
   }
-  const nums = numbers.split(/[\n,]/);
+  let delimiter = /[\n,]/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].substring(2));
+    numbers = parts[1];
+  }
+  const nums = numbers.split(delimiter);
   return nums.reduce((sum, num) => {
     return sum + parseInt(num);
   }, 0);

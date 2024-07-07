2898f8fc8f295c1ca81541119c6956ac4a0a28c5
pratikgofynd <pratikhadvani@gofynd.com>
2024-07-07 13:59:17 +0530

    added case for single number and updated code accordingly

--- __tests__/string-calculator.test.js
+++ __tests__/string-calculator.test.js
@@ -3,3 +3,7 @@ import add from "./../string-calculator.js";
 test("return 0 for an emoty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("return number itself when single number as a string", () => {
+  expect(add("1")).toBe(1);
+});
